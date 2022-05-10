52a2d31914757cded4a73f37a0274db5a1678e8c
vessemer <vessemer@gmail.com>
2022-05-10 12:20:14 +0000

    Corr model

--- data/callpeaks.sh
+++ data/callpeaks.sh
@@ -5,5 +5,5 @@ macs2 callpeak -t input/REP2.bam -c control/CONTROL.bam -n REP2 --outdir peaks >
 macs2 callpeak -t 'input/REP1.bam input/REP2.bam' -c control/CONTROL.bam -q 0.01 -n REPS --outdir peaks > peaks/REPS_all.log 2>&1 &
 
 # Clean blacklisted entries 
-bedtools subtract -A -a peaks/REPS_peaks.narrowPeak -b blacklist/hg38.blacklist.bed.gz > peaks/REPS_blacklisted.bed
+bedtools subtract -A -a peaks/REPS_peaks.narrowPeak -b blacklist/hg38.blacklist.bed > peaks/REPS_blacklisted.bed
 
